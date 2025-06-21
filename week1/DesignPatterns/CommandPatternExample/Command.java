fe39dd197a8185c1ef2c4e49d68b40540db4f36e
wisam-kamal <wisamkamal0108@gmail.com>
2025-06-21 13:57:59 +0530

    Add files via upload

---
+++ week1/DesignPatterns/CommandPatternExample/Command.java
@@ -0,0 +1,53 @@
+interface Command {
+    void execute();
+}
+
+class Light {
+    public void turnOn() {
+        System.out.println("The light is ON.");
+    }
+
+    public void turnOff() {
+        System.out.println("The light is OFF.");
+    }
+}
+
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        if (command != null) {
+            command.execute();
+        } else {
+            System.out.println("No command set.");
+        }
+    }
+}
