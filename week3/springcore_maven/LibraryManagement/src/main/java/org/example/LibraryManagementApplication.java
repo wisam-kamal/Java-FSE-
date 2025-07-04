b72a754c5b54f295697469b1f257b31391bf6e2a
wisam-kamal <wisamkamal0108@gmail.com>
2025-07-04 13:10:12 +0530

    Add files via upload

---
+++ week3/springcore_maven/LibraryManagement/src/main/java/org/example/LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean(BookService.class);
+        bookService.addBook();
+    }
+}
