2d9f748d36eb9e7ac87b9028c4d80533b4debe3c
wisam-kamal <wisamkamal0108@gmail.com>
2025-07-11 20:45:12 +0530

    Update SpringLearnApplication.java

--- week4/REST/Spring_learn/SpringLearnApplication.java
+++ week4/REST/Spring_learn/SpringLearnApplication.java
@@ -1,9 +1,17 @@
 package com.cognizant.springlearn;
 
+import com.cognizant.springlearn.model.Country;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -13,7 +21,42 @@ public class SpringLearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
         LOGGER.info("START");
-        LOGGER.info("Inside main");
+
+        displayDate();
+        displayCountry();
+        displayCountries();
+
+        LOGGER.info("END");
+    }
+
+    public static void displayDate() {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        try {
+            Date parsedDate = format.parse("31/12/2018");
+            LOGGER.debug("Parsed Date: {}", parsedDate.toString());
+        } catch (ParseException e) {
+            LOGGER.error("Error parsing date", e);
+        }
+        LOGGER.info("END");
+    }
+
+    public static void displayCountry() {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        Country anotherCountry = context.getBean("country", Country.class);
+        LOGGER.debug("Country: {}", country);
+        LOGGER.debug("Another Country: {}", anotherCountry);
+        LOGGER.info("END");
+    }
+
+    public static void displayCountries() {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countries = context.getBean("countryList", List.class);
+        LOGGER.debug("Country List: {}", countries);
         LOGGER.info("END");
     }
 }
