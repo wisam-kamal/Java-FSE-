1db4d6692a7946cd423395ba5cff58afa35a03b1
wisam-kamal <wisamkamal0108@gmail.com>
2025-07-11 20:42:07 +0530

    Add files via upload

---
+++ week4/REST/Spring_learn/SpringLearnApplication.java
@@ -0,0 +1,19 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        LOGGER.info("START");
+        LOGGER.info("Inside main");
+        LOGGER.info("END");
+    }
+}
