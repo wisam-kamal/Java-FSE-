ade69d4e5b91b82d4779082c7680e82f383cfa45
wisam-kamal <wisamkamal0108@gmail.com>
2025-07-18 16:39:19 +0530

    Add files via upload

---
+++ week5/microservices/InventoryManagement/EurekaServer/DiscoveryServerApp.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApp.class, args);
+    }
+}
